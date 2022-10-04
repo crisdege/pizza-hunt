c081e29fd3ae708930eaed7a9216b69477cdb91c
Cristina Dege <cris.dege0813@gmail.com>
2022-10-04 10:20:42 -0400

    deplpoy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dbPizza',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/dbPizza',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
